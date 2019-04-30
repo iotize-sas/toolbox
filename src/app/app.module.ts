2df41993e008341ead967208bfaf1abee80fcb2e
dev@iotize.com <developper@MacBook-Air-IoTize.local>
2019-04-30 17:27:21 +0200

    first commit after init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,16 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { IotizeNgComModule } from "iotize-ng-com";
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    IotizeNgComModule
   ],
   providers: [
     StatusBar,
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IoTizeBle, IoTizeTap, DiscoveredDeviceType } from 'iotize-ng-com';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,69 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(public ble: IoTizeBle,
+    public tapService: IoTizeTap,
+    private toast: ToastController) { }
 
+  devices: DiscoveredDeviceType[] = [];
+
+  startScan() {
+    this.ble.startScan().subscribe(
+      device => {
+        console.log(device);
+        if (this.devices.findIndex(dev => dev.address == device.address) == -1) {
+          this.devices.push(device);
+        }
+      });
+  }
+
+  stopScan() {
+    this.ble.stopScan();
+  }
+
+  async connect(device: DiscoveredDeviceType) {
+     const connectionProtocol = this.ble.getProtocol(device);
+     try {
+       await this.tapService.init(connectionProtocol);
+     } catch (error) {
+       this.handleError(error);
+     }
+  }
+
+  disconnect() {
+    return this.tapService.disconnect();
+  }
+
+  clear() {
+    if (this.tapService.tap) {
+      // this.devices.filter(device => device.address === this.)
+    } else {
+      this.devices.splice(0);
+    }
+    this.tapService.clear();
+  }
+  
+  refreshDevices() {
+    console.log("refreshing devices");
+  }
+
+  private handleError(error: any) {
+    let errorString = '';
+    if (typeof error === "string") {
+      errorString = error;
+    } else if (error.message) {
+      errorString = error.message;
+    } else {
+      errorString = error.toString();
+    }
+    this.showError(errorString);
+  }
+
+  private showError(errorMessage: string) {
+    this.toast.create({
+      message: errorMessage,
+      duration: 0,
+      showCloseButton: true
+    })
+  }
 }
