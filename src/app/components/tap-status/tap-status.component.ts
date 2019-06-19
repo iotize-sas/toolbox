ea85bd87be6f2be64d4e95c1dbd37c9d52f63b3d
GVercasson <geraud.vercasson@iotize.com>
2019-06-19 18:15:38 +0200

    login moved in tap-status component

--- src/app/components/tap-status/tap-status.component.ts
+++ src/app/components/tap-status/tap-status.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectorRef } from '@angular/core';
 import { TapService } from 'src/app/services/tap.service';
-import { Events } from '@ionic/angular';
+import { Events, AlertController, LoadingController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'tap-status',
@@ -9,11 +9,15 @@ import { Events } from '@ionic/angular';
 })
 export class TapStatusComponent implements OnInit {
 
-    appName: string;
-    userName: string;
+    appName: string = '';
+    userName: string = '';
 
   constructor(public tapService: TapService,
-    public events: Events) { }
+    public events: Events,
+    public alertCtrl: AlertController,
+    public changeDetector: ChangeDetectorRef,
+    public loadingCtrl: LoadingController,
+    public toastController: ToastController) { }
 
   ngOnInit() {
     this.events.subscribe('connected',() => {
@@ -24,7 +28,125 @@ export class TapStatusComponent implements OnInit {
 
   async makeBinds() {
     this.appName = (await this.tapService.tap.service.interface.getAppName()).body();
-    this.tapService.tap.sessionState.subscribe(_ => this.userName = _.name);
+    this.tapService.tap.sessionState.subscribe(_ => {
+      this.userName = _.name;
+      this.changeDetector.detectChanges();
+    });
+  }
+
+  async openLoginAlert() {
+    const alert = await this.alertCtrl.create({
+      header: 'Login',
+      inputs: [
+        {
+          name: 'username',
+          type: 'text',
+          placeholder: 'Username'
+        },
+        {
+          name: 'password',
+          type: 'password',
+          placeholder: 'Password'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => { }
+        },
+        {
+          text: 'Ok',
+          handler: async (data) => {
+            alert.dismiss();
+            await this.login(data['username'], data['password']);
+            this.changeDetector.detectChanges();
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  async login(username: string, password: string) {
+    const loader = await this.loadingCtrl.create({ message: 'Logging in' });
+    loader.present();
+
+    try {
+      const logSuccess = await this.tapService.login(username, password);
+      loader.dismiss();
+      if (logSuccess) {
+        this.showToast(`Logged in as ${username}`);
+      } else {
+        this.showToast(`Wrong username or password`);
+      }
+
+    } catch (error) {
+      loader.dismiss();
+      console.error(error);
+      this.showClosingToast(`Login error : ${error.message? error.message: error}`);
+    }
+
+  }
+
+  async openLogoutAlert() {
+    const alert = await this.alertCtrl.create({
+      header: 'Logout',
+      message: 'Are you sure?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => { }
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            alert.dismiss();
+            this.logout();
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  async logout() {
+    const loader = await this.loadingCtrl.create({ message: 'Logging out' });
+    loader.present();
+
+    try {
+      const logSuccess = await this.tapService.logout();
+      loader.dismiss();
+      if (logSuccess) {
+        this.showToast(`Logged out`);
+      } else {
+        this.showToast(`Could not log out`);
+      }
+
+    } catch (error) {
+      loader.dismiss();
+      console.error(error);
+      this.showClosingToast(`Logout error : ${error}`);
+    }
+  }
+
+  async showToast(message: string, duration: number = 3000): Promise<void> {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: duration
+    });
+
+    toast.present();
+  }
+
+  async showClosingToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      showCloseButton: true,
+    });
+
+    toast.present();
   }
 
 }
