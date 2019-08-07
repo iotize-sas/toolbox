8bf921de55deea490bc634a4bcc4df79fb9e0c9c
Behnaz Bouchiat <behnaz.bouchiat@iotize.com>
2019-08-07 14:28:29 +0200

    change native status bar colors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -50,7 +50,7 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
+      this.statusBar.styleLightContent();
       this.splashScreen.hide();
     });
   }
