6163bf833986b6ee2c127a5a8e6bdaf2df043929
Behnaz Bouchiat <behnaz.bouchiat@iotize.com>
2019-09-10 10:05:13 +0200

    communication test

--- src/app/pages/infos/infos.page.ts
+++ src/app/pages/infos/infos.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { InfosService } from 'src/app/services/infos.service';
-import { Subscription } from 'rxjs';
+import { Subscription, timer, Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { StringConverter } from '@iotize/device-client.js/client/impl';
 
 @Component({
   selector: 'app-infos',
@@ -21,5 +23,24 @@ export class InfosPage  {
   ionViewWillLeave() {
     this._subscription.unsubscribe();
     this.tapInfos = null;
+  }
+
+  count = 0;
+  start = new Date();
+  end;
+  diff;
+
+  launchTestTask() {
+    this.count = 0;
+    this.start = new Date();
+    const source = timer(0, 500);
+    const mySubject = new Subject();
+    const mySub = source.subscribe(() => {
+      this.infos.tapService.tap.service.target.send(StringConverter.instance().encode('test'))
+        .then(_ => this.infos.tapService.tap.service.target.readBytes()
+          .then(_ => mySubject.next()).catch(_ => mySubject.error('command readBytes failed')))
+        .catch(_ => mySubject.error('command send failed'));
+    });
+     mySubject.subscribe(() => {this.count+=2}, err => {console.error(err); this.end = new Date(); this.diff = (this.end-(this.start as any))/1000;});
   }
 }
